d4e6848300e880e7b1e868454d0e38c653dc537a
basilkot <basilkot@gmail.com>
2019-06-13 10:49:20 +0200

    remove unused scripts

--- theme/assets/js/main.js
+++ theme/assets/js/main.js
@@ -1,6 +1,6 @@
-$(function (){
+$(function () {
 
-    $('.header .nav__item').on('click', function() {
+    $('.header .nav__item').on('click', function () {
         var self = $(this);
 
         $('.dropdown-overlay').remove();
@@ -9,7 +9,7 @@ $(function (){
         $('.header .nav__item').removeClass('nav__item--active');
         $('.dropdown-menu').removeClass('opened');
 
-        if(self.hasClass('nav__item--active')) {
+        if (self.hasClass('nav__item--active')) {
             self.removeClass('nav__item--active');
             $('.dropdown-menu', self).removeClass('opened');
             $('.dropdown-overlay').remove();
@@ -20,29 +20,35 @@ $(function (){
         }
     });
 
-    $('.header-toggle').on('click', function() {
+    $('.header-toggle').on('click', function () {
         $('body').addClass('swipe-open').prepend('<div class="overlay"></div>');
         $('.swipe').addClass('opened');
     });
 
-    $('.swipe__close').on('click', function() {
+    $('.swipe__close').on('click', function () {
         $('body').removeClass('swipe-open');
         $('.swipe').removeClass('opened');
         $('.overlay').remove();
     });
 
-    $('.nav__label').on('click', function() {
+    $(':not(.nav__content) .nav__item').on('click', function () {
+        var self = $(this);
+        self.addClass('nav__item--active').siblings().removeClass('nav__item--active').addClass('nav__item--animated');
+        self.removeClass('nav__item--animated');
+        $('.nav__content').removeClass('nav__content--opened').eq(self.index()).addClass('nav__content--opened');
+
         $('.swipe__nav-item').removeClass('swipe__nav-item--show');
         $('.swipe__nav-item:nth-child(2)').addClass('swipe__nav-item--show');
     });
 
-    $('.swipe__back').on('click', function() {
+    $('.swipe__back').on('click', function () {
         $('.swipe__nav-item').removeClass('swipe__nav-item--show');
         $('.swipe__nav-item:nth-child(1)').addClass('swipe__nav-item--show');
-        $('.swipe .nav__input').prop('checked', false);
+        $('.nav__content').removeClass('nav__content--opened');
+        $('.nav__item').removeClass('nav__item--animated nav__item--active');
     });
 
-    $('body').delegate('.dropdown-overlay', 'click', function() {
+    $('body').delegate('.dropdown-overlay', 'click', function () {
         $('.header .nav__item').removeClass('nav__item--active');
         $('.dropdown-menu').removeClass('opened');
         $('.dropdown-overlay').remove();
