5a514e7b39400081daaa43b3859995772d6835d5
Sergey Podguzov <spo@virtoway.com>
2019-07-31 18:52:09 +0500

    FE: Case study form validation (New Design).

--- theme/assets/scripts/angular/casestudy.js
+++ theme/assets/scripts/angular/casestudy.js
@@ -1,10 +1,17 @@
-var storefrontApp = angular.module('storefrontApp');
+var storefrontApp = angular.module('storefrontApp', []);
 
 storefrontApp.controller('caseStudyController', ['$scope', function ($scope) {
 
     $scope.emailPattern = new RegExp(/((^|((?!^)([,;]|\r|\r\n|\n)))([a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*))+$/);
 
-    $scope.caseStudy = {}
+    $scope.caseStudy = {};
+
+    $scope.submit = function (fileUrl) {
+        if ($scope.caseStudyForm.$valid)
+            window.location.href = fileUrl;
+        else
+            event.preventDefault();
+    };
 
     $scope.setCaseStudyForm = function (form) {
         $scope.caseStudyForm = form;
