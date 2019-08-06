4b4afeee185e67b8a86d8f14df431c707b122c80
Basil Kotov <basilkot@gmail.com>
2019-08-06 09:35:16 +0200

    change team script

--- theme/assets/js/team.js
+++ theme/assets/js/team.js
@@ -1,54 +1,74 @@
 // list--pics
 // list__item
-$(function () {
+(function () {
     var rows = 3;
     var interval = 10000;
-    var container = $('.list--pics');
-    if (container.length) {
-        var items = $('.list__item', container);
-        var images = $('.list__item img', container);
-        //images.hide();
-        //images.each(function () {
-        //    var $img = $(this);
-        //    $img.removeClass('list__pic');
-        //    this.onload = function () {
-        //        $img.show();
-        //        $img.addClass('list__pic');
-        //        console.log(1);
-        //    };
-        //});
-        var countInRow = 0;
-        var top = 0;
-        var visibleUrls = [];
-        items.each(function () {
+
+    $(function () {
+        var container = $('.list--pics');
+        var context = {
+            container: container,
+            items: null,
+            images: null,
+            visibleUrls: [],
+            invisibleUrls: [],
+            timer: null
+        };
+        if (container.length) {
+            context.source = container.html();
+
+            var timer = null;
+            window.onresize = function () {
+                context.container.animate({ opacity: 0 }, { duration: 100, queue: false });
+                if (timer) {
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(function () {
+                    init(context);
+                }, 1000);
+            };
+
+            init(context);
+        }
+    });
+
+    function init(context) {
+        context.visibleUrls = [];
+        context.container.children().remove();
+        context.container.html(context.source);
+        context.items = $('.list__item', context.container);
+        context.images = $('.list__item img', context.container);
+        context.items.each(function () {
             var $this = $(this);
-            if (top === 0) {
-                top = $this.offset().top;
-            }
-            if ($this.offset().top === top) {
-                countInRow++;
-            }
-            visibleUrls.push($this.children().attr('src'));
+            context.visibleUrls.push($this.children().attr('src'));
         });
+        var containerWidth = context.container.width();
+        var singleItem = $(context.items.get(0));
+        var itemWidth = singleItem.width() + parseInt(singleItem.css('margin')) * 2;
+        var countInRow = Math.floor(containerWidth / itemWidth);
         var displayCount = countInRow * rows;
-        items.each(function (index) {
+        context.items.each(function (index) {
             if (index >= displayCount) {
                 $(this).remove();
             }
         });
-        var invisibleUrls = visibleUrls.splice(displayCount, visibleUrls.length - displayCount);
-        container.animate({ opacity: 1 }, 1000);
-        setInterval(function () {
-            var newImg = Math.floor(invisibleUrls.length * Math.random());
-            var oldImg = Math.floor(visibleUrls.length * Math.random());
-            var newUrl = invisibleUrls[newImg];
-            invisibleUrls[newImg] = visibleUrls[oldImg];
-            visibleUrls[oldImg] = newUrl;
-            var $img = $(images.get(oldImg));
+        context.invisibleUrls = context.visibleUrls.splice(displayCount, context.visibleUrls.length - displayCount);
+        context.container.animate({ opacity: 1 }, 1000);
+        if (context.timer) {
+            clearInterval(context.timer);
+        }
+        context.timer = setInterval(function () {
+            var newImg = Math.floor(context.invisibleUrls.length * Math.random());
+            var oldImg = Math.floor(context.visibleUrls.length * Math.random());
+            var newUrl = context.invisibleUrls[newImg];
+            context.invisibleUrls[newImg] = context.visibleUrls[oldImg];
+            context.visibleUrls[oldImg] = newUrl;
+            var $img = $(context.images.get(oldImg));
             $img.animate({ opacity: 0 }, 1000, function () {
                 $img.attr('src', newUrl);
                 $img.animate({ opacity: 1 }, 1000);
             });
         }, interval);
+
     }
-});
+})();
